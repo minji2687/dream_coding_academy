2c87d1167a9ebc255057c21e683067c06bf58da4
Jo Minji <mingi2687@gamil.com>
2020-10-24 16:06:14 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './app.css';
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <h1>hello :)</h1>
-  );
+  const [videos, setvideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&chart=mostPopular&key=AIzaSyAh32w91Dsh7-u4PwWq2YFRyH0-ZePFwUw",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setvideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
